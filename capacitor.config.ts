9d14a28a764a870117684c883332c34aaba31873
Leo-March <leonardomarcheti2@gmail.com>
2025-11-03 14:45:31 -0300

    to tentando atualizar aq

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'mobile-pragasCana',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'http://192.168.75.1',
+    cleartext: true
+  }
 };
 
 export default config;
